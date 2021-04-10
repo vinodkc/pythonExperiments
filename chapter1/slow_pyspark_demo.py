95c0a12511f2648a9172109e133c4886d4f9cad7
Vinod KC <vinod.kc.in@gmail.com>
2021-04-10 10:55:19 +0530

    added slow python

---
+++ chapter1/slow_pyspark_demo.py
@@ -0,0 +1,14 @@
+import sys
+import time
+from pyspark.sql import SparkSession
+def main():
+    # Wait for 3 mins before starting Spark Session
+    time.sleep(180)
+    spark = SparkSession.builder.appName("pyspark_Demo.py").enableHiveSupport().getOrCreate()
+    words = spark.sparkContext.parallelize (["scala","java","hadoop","spark","akka","spark vs hadoop","pyspark","pyspark and spark"])
+
+    counts = words.count()
+    print "Number of elements in RDD -> %i" % (counts)
+
+if __name__ == '__main__':
+   main()
